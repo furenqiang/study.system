984c1ea1f91ec9b015d85a83dc8c71ca345e0fe1
furenqiang <www.1726894668@foxmail.com>
2023-12-07 18:10:41 +0800

    springboot3.x初始化

--- src/main/java/com/furenqiang/system/SystemApplication.java
+++ src/main/java/com/furenqiang/system/SystemApplication.java
@@ -1,25 +1,22 @@
 package com.furenqiang.system;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@MapperScan(value = {"com.furenqiang.system.mapper"})
+//@MapperScan(value = {"com.furenqiang.system.mapper"})
 //@ComponentScan(basePackages = "com.furenqiang.system")
-@EnableSwagger2
+//@EnableSwagger2
 //@EnableAutoConfiguration
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SystemApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SystemApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        var run = SpringApplication.run(SystemApplication.class, args);
         logger.info("========================服务启动完毕========================");
     }
 
--- src/main/java/com/furenqiang/system/service/SysMenuService.java
+++
@@ -1,9 +0,0 @@
-package com.furenqiang.system.service;
-
-import com.furenqiang.system.common.ResponseResult;
-
-public interface SysMenuService {
-
-    ResponseResult getMenuTree();
-
-}
